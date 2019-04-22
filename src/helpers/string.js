428b34c78824df29e3653f59885a4d328b1fccf4
Adam Sinning <asinning25@gmail.com>
2019-04-22 20:01:58 -0300

    capitalize function refactored

--- src/helpers/string.js
+++ src/helpers/string.js
@@ -1,9 +1,6 @@
 export function capitalize(word) {
-  if (word === null) {
-    return null;
-  }
-  if (word === "") {
-    return "";
+  if (!word) {
+    return word;
   }
   return word[0].toUpperCase() +
     word.substr(1).toLowerCase();
